49593fbf4863b8b8cfc43cdc99a715e1a9dc8dd3
RakshithAjila <rrakshithshetty2797@gmail.com>
2025-10-29 23:04:46 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,57 @@
-main.tf
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins-sg"
+  description = "Allow SSH and Jenkins"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Jenkins"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "jenkins-sg"
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_instance" "jenkins_server" {
+  ami                    = "ami-02d26659fd82cf299" # Ubuntu 22.04 (change region if needed)
+  instance_type          = "t3.micro"
+  key_name               = "rakshithpravkey" # replace with your key pair name
+  subnet_id              = element(data.aws_subnet_ids.default.ids, 0)
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+
+  tags = {
+    Name = "jenkins-server"
+  }
+}
+
+output "jenkins_public_ip" {
+  value = aws_instance.jenkins_server.public_ip
+}
+
