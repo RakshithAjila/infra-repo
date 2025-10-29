68679fcb1d6af31bea02a59495e33e2cdc40c16f
RakshithAjila <rrakshithshetty2797@gmail.com>
2025-10-29 23:02:05 +0530

    Set up AWS provider and local backend in provider.tf
    Configure AWS provider and local backend for Terraform.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,1 +1,20 @@
-provider.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# optional backend (if using local state)
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
